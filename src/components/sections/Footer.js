07a10138958093efaa11e083f54098b754bc2607
Rahim Ratnani <rahimratnani4@gmail.com>
2021-02-01 08:34:24 +0530

    basic homepage ready

---
+++ src/components/sections/Footer.js
@@ -0,0 +1,33 @@
+import { Box, Divider, Flex, Spacer, Text, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import SocialMedia from "../ui/SocialMedia";
+
+const Footer = () => {
+  return (
+    <Flex
+      as="footer"
+      direction="column"
+      align="center"
+      justify="center"
+      h="full"
+      w={{ base: "90%", lg: "80%", xl: "1040px" }}
+    >
+      {/* <Text fontWeight="500" mb="1rem">
+        &copy; {new Date().getFullYear()} - Rahim Ratnani
+      </Text> */}
+      <Link
+        to="/"
+        _hover={{ textDecoration: "none", color: "primary" }}
+        fontWeight="500"
+        mb="1rem"
+        as={RouterLink}
+      >
+        &copy; {new Date().getFullYear()} - Rahim Ratnani
+      </Link>
+
+      <SocialMedia direction="row" />
+    </Flex>
+  );
+};
+
+export default Footer;
