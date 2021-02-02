db555ac5d25c5e8a72c3494b2c2ee6ffac351054
Rahim Ratnani <rahimratnani4@gmail.com>
2021-02-02 14:53:39 +0530

    add photo and heading in about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,19 +3,26 @@ import Photo from "../ui/Photo";
 
 const About = () => {
   return (
-    <Flex
-      borderColor="black"
-      borderStyle="solid"
-      border="1px"
-      direction="column"
-      w={{ base: "90%", lg: "80%", xl: "1040px" }}
-      mt=""
-      //   align="center"
-    >
-      <Heading fontWeight="700" fontSize={{ base: "5xl" }} as="h1">
-        About Me
-      </Heading>
-      <Photo />
+    <Flex direction="row" w="100%" justify="center" as="main">
+      {/* =================== */}
+
+      <Flex
+        className="container"
+        borderColor="black"
+        borderStyle="solid"
+        border="1px"
+        direction="column"
+        w={{ base: "90%", lg: "80%", xl: "1040px" }}
+        mt=""
+        align="center"
+      >
+        <Heading fontWeight="700" fontSize={{ base: "5xl" }} as="h1">
+          About Me
+        </Heading>
+        <Photo />
+      </Flex>
+
+      {/* =================== */}
     </Flex>
   );
 };
